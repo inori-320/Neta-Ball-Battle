a276d3754b97f3165e89ff409826b33c7b8f123c
後藤　独 <719471785@qq.com>
2023-01-16 21:05:58 +0800

    Frame classification

---
+++ scripts/merge_game_js.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+path=/home/lty/diploma_project/game/static/js/
+js_path_dist=${path}dist/
+js_path_src=${path}src/
+
+find $js_path_src -type f -name '*.js' | sort | xargs cat > ${js_path_dist}game.js
