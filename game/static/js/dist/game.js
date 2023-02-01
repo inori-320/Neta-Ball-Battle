5bb2053762aafb714553978738f693fa702923e9
後藤　独 <719471785@qq.com>
2023-02-01 18:13:42 +0800

    add login and register

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -451,6 +451,102 @@ class GamePlayground{
 class GameSettings{
     constructor(root){
         this.root = root;
+        this.$settings = $(`
+<div class = "game_settings">
+    <div class = "game_settings_login">
+        <div class = "game_settings_title">
+        登录
+        </div>
+        <div class = "game_settings_username">
+            <div class = "game_settings_item">
+                <input type = "text" placeholder = "用户名">
+            </div>
+        </div>
+        <div class = "game_settings_passwd">
+            <div class = "game_settings_item">
+                <input type = "password" placeholder = "密码">
+            </div>
+        </div>
+        <div class = "game_settings_affirm">
+            <div class = "game_settings_item">
+                <button>确认</button>
+            </div>
+        </div>
+        <div class = "game_settings_error">
+        </div>
+        <div class = "game_settings_choice">
+            注册
+        </div>
+        <br>
+        <div class = "game_settings_qq">
+            <img width = "40" src = "http://8.130.15.181:8000/static/image/settings/qq_logo.png">
+            <br>
+            <div class = "game_settings_qq_title">
+                QQ一键登录
+            </div>
+        </div>
+    </div>
+    <div class = "game_settings_register">
+        <div class = "game_settings_title">
+            注册
+        </div>
+        <div class = "game_settings_username">
+            <div class = "game_settings_item">
+                <input type = "text" placeholder = "用户名">
+            </div>
+        </div>
+        <div class = "game_settings_passwd">
+            <div class = "game_settings_item">
+                <input type = "password" placeholder = "密码">
+            </div>
+        </div>
+        <div class = "game_settings_comfirm_passwd">
+            <div class = "game_settings_item">
+                <input type = "password" placeholder = "确认密码">
+            </div>
+        </div>
+        <div class = "game_settings_affirm">
+            <div class = "game_settings_item">
+                <button>确认</button>
+            </div>
+        </div>
+        <div class = "game_settings_error">
+        </div>
+        <div class = "game_settings_choice">
+            登录
+        </div>
+        <br>
+        <div class = "game_settings_qq">
+            <img width = "40" src = "http://8.130.15.181:8000/static/image/settings/qq_logo.png">
+            <br>
+            <div class = "game_settings_qq_title">
+                QQ一键注册
+            </div>
+        </div>
+    </div>
+</div>
+            `);
+
+        this.$login = this.$settings.find(".game_settings_login");
+        this.$login_username = this.$login.find(".game_settings_username input");
+        this.$login_passwd = this.$login.find(".game_settings_passwd input");
+        this.$login_affirm = this.$login.find(".game_settings_affirm button");
+        this.$login_error = this.$login.find(".game_settings_error");
+        this.$login_register = this.$login.find(".game_settings_choice");
+
+        this.$login.hide();
+
+        this.$register = this.$settings.find(".game_settings_register");
+        this.$register_username = this.$register.find(".game_settings_username input");
+        this.$register_passwd_1 = this.$register.find(".game_settings_passwd input");
+        this.$register_passwd_2 = this.$register.find(".game_settings_comfirm_passwd input");
+        this.$register_affirm = this.$register.find(".game_settings_affirm button");
+        this.$register_error = this.$register.find(".game_settings_error");
+        this.$register_login = this.$register.find(".game_settings_choice");
+
+        this.$register.hide();
+
+        this.root.$lty.append(this.$settings);
         this.platform = "WEB";
         this.username = "";
         this.photo = "";
@@ -460,12 +556,36 @@ class GameSettings{
 
     start(){
         this.get_info();
+        this.listening_events();
+    }
+
+    listening_events(){
+        this.listening_events_login();
+        this.listening_events_register();
+    }
+
+    listening_events_login(){
+        let outer = this;
+        this.$login_register.click(function(){
+            outer.register();
+        });
+    }
+
+    listening_events_register(){
+        let outer = this;
+        this.$register_login.click(function(){
+            outer.login();
+        });
     }
 
     register(){
+        this.$login.hide();
+        this.$register.show();
     }
 
     login(){
+        this.$register.hide();
+        this.$login.show();
     }
 
     get_info(){
@@ -491,9 +611,11 @@ class GameSettings{
     }
 
     show(){
+        this.$settings.show();
     }
 
     hide(){
+        this.$settings.hide();
     }
 }
 
